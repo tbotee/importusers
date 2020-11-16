a7663dd27c23104a0563140bdd26c0be2ffea2f9
tbotee <tbotee@yahoo.com>
2020-11-16 23:04:03 +0200

    Random user generator added

---
+++ app/Console/Commands/GenerateUsers.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Faker\Factory as Faker;
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class GenerateUsers extends Command
+{
+
+    private $nrOfUsers  = 100000;
+    private $fileName   = 'userList.csv';
+
+    protected $signature = 'command:generate_users';
+    protected $description = 'Generates 100k users and exports them into a csv file';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->generateCSV();
+    }
+
+    private function generateCSV()
+    {
+        $fp = fopen($this->fileName, 'w');
+
+        $this->writeUsersToCSV($fp);
+
+        fclose($fp);
+    }
+
+    private function writeUsersToCSV($fp): void
+    {
+        $faker = Faker::create();
+        foreach (range(1, $this->nrOfUsers) as $nr) {
+            fputcsv($fp, User::getRandomUserForCSV($faker));
+        }
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Faker;
 
 class User extends Authenticatable
 {
@@ -42,4 +42,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getRandomUserForCSV(Faker\Generator $faker)
+    {
+        return array(
+            $faker->name,
+            $faker->safeEmail,
+            $faker->password(6,10),
+            $faker->phoneNumber
+        );
+    }
 }
